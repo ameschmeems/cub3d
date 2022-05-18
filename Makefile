004e00b6b3eb0defd9752e1d2474d6054f3c2819
Kacper Pucylo <pucylek@wp.pl>
2022-05-18 14:24:38 +0200

    first

---
+++ Makefile
@@ -0,0 +1,27 @@
+NAME	:=	cub3d
+
+SRCS	:=	main.c
+
+OBJS	:=	${SRCS:.c=.o}
+
+CC		:=	gcc
+
+CFLAGS	:=	-Wall -Wextra -Werror
+
+all:		${NAME}
+
+%.o: %.c
+		${CC} -Wall -Wextra -Werror -Imlx -c $< -o $@
+
+${NAME}:	${OBJS}
+		${CC} ${OBJS} -Lmlx -lmlx -framework OpenGL -framework AppKit -o ${NAME}
+
+clean:
+		rm -f ${OBJS} ${FPS_OBJS}
+
+fclean:		clean
+		rm -f ${NAME} ${FPS_NAME}
+
+re:			fclean all
+
+.PHONY:		all clean fclean re
---
+++ init.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kpucylo <kpucylo@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/18 12:49:09 by kpucylo           #+#    #+#             */
+/*   Updated: 2022/05/18 14:02:41 by kpucylo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "cub3d.h"
+
+void	set_vectors(t_data *data)
+{
+}
+
+t_data	*init(void)
+{
+	t_data	*data;
+
+	data = malloc(sizeof(t_data));
+	data->mlx = mlx_init();
+	data->win = mlx_new_window(data->mlx, WIDTH, HEIGHT, "cub3d");
+	data->map_img.img = mlx_new_image(data->mlx, MAP_W, MAP_H);
+	data->map_img.addr = mlx_get_data_addr(data->map_img.img, \
+		&data->map_img.bits_per_pixel, \
+		&data->map_img.line_length, &data->map_img.endian);
+	data->fps.img = mlx_new_image(data->mlx, WIDTH, HEIGHT);
+	data->fps.addr = mlx_get_data_addr(data->fps.img, \
+		&data->fps.bits_per_pixel, \
+		&data->fps.line_length, &data->fps.endian);
+	//map here maybe
+	set_vectors(data);
+	return (data);
+}
