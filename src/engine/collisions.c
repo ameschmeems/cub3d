b3a5fa8387ce6787e2c8b3fc321793472a5e0bc7
Kacper Pucylo <pucylek@wp.pl>
2022-05-26 14:56:42 +0200

    some stuff

--- src/engine/collisions.c
+++ src/engine/collisions.c
@@ -6,7 +6,7 @@
 /*   By: kpucylo <kpucylo@student.42wolfsburg.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 12:39:36 by kpucylo           #+#    #+#             */
-/*   Updated: 2022/05/24 14:36:45 by kpucylo          ###   ########.fr       */
+/*   Updated: 2022/05/26 14:55:16 by kpucylo          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,23 @@ int	collision_detection(t_data *data, int state)
 {
 	if (state == FORWARD)
 	{
-		data->map_x = (data->pos.x + 20 * data->dir.x) / 100;
-		data->map_y = (data->pos.y + 20 * data->dir.y) / 100;
+		data->map_x = (data->pos.x + 15 * data->dir.x) / 100;
+		data->map_y = (data->pos.y + 15 * data->dir.y) / 100;
 	}
 	else if (state == BACK)
 	{
-		data->map_x = (data->pos.x - 20 * data->dir.x) / 100;
-		data->map_y = (data->pos.y - 20 * data->dir.y) / 100;
+		data->map_x = (data->pos.x - 15 * data->dir.x) / 100;
+		data->map_y = (data->pos.y - 15 * data->dir.y) / 100;
 	}
 	else if (state == LEFT)
 	{
-		data->map_x = (data->pos.x + 20 * data->perp_dir.x) / 100;
-		data->map_y = (data->pos.y + 20 * data->perp_dir.y) / 100;
+		data->map_x = (data->pos.x + 15 * data->perp_dir.x) / 100;
+		data->map_y = (data->pos.y + 15 * data->perp_dir.y) / 100;
 	}
 	else if (state == RIGHT)
 	{
-		data->map_x = (data->pos.x - 20 * data->perp_dir.x) / 100;
-		data->map_y = (data->pos.y - 20 * data->perp_dir.y) / 100;
+		data->map_x = (data->pos.x - 15 * data->perp_dir.x) / 100;
+		data->map_y = (data->pos.y - 15 * data->perp_dir.y) / 100;
 	}
 	if (data->map[data->map_y][data->map_x] == '1' || \
 		data->map[data->map_y][data->map_x] == ' ')
